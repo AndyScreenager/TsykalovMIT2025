460d10a6f154390dd3acdb2a970ee52525e050de
AndyScreenager <this.is.a.real.mail.write.to.me@gmail.com>
2025-03-16 20:20:20 +0400

    Commit2

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+def fib(n: int) -> int:
+    if n == 1 or n == 2:
+        return 1
+    return fib(n - 1) + fib(n - 2)
+
+
 print('Hello, World!')
+n = int(input('n = '))
+print(f'fib({n}) = {fib(n)}')
